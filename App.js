ac2c224aace4b739c6ce7c95adb59f63e1676da4
MartinHwang <hic0811@gmail.com>
2020-07-22 08:59:20 -0400

    React_native Android for martinhwangs.com

---
+++ App.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2020 Martin Hwang
+ * All rights reserved.
+ *
+ * Filename: App.js
+ *
+ * Key Options:
+ * -
+ *
+ * Revision History:
+ * - 05 July 2020, Queenie <developer.martinhwang@gmail.com>: Created
+ */
+
+import React, {Fragment, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>martinhwangs.com</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+    textAlign: 'center'
+  },
+});
+
+export default App;
